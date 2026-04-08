6bacc3107114e39a32a6f181e8fc9830af4df09a
dylan-m-mitchell <dlnmtchll@gmail.com>
2026-04-07 23:18:50 -0500

    feat: add initial contract setup with AssetTransferContract

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { type Contract } from 'fabric-contract-api';
+import { AssetTransferContract } from './assetTransfer';
+
+export const contracts: typeof Contract[] = [AssetTransferContract];
